122d1da54e9cdab728b1d525e42a7bf89953b08e
Fifaliana <hei.fifaliana@gmail.com>
2024-02-22 12:30:53 +0300

    feat: upload image to bucket

--- src/main/java/hei/school/sarisary/service/SaryService.java
+++ src/main/java/hei/school/sarisary/service/SaryService.java
@@ -1,24 +1,46 @@
 package hei.school.sarisary.service;
 
+import hei.school.sarisary.file.BucketComponent;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SaryService {
-  public byte[] ConvertSary(byte[] sary, String id) throws IOException {
+  private final BucketComponent bucketComponent;
 
-    BufferedImage image = ImageIO.read(new ByteArrayInputStream(sary));
+  private File uploadImage(String file, BufferedImage image) throws IOException {
+    String allName = file + " .png";
+    File tempFile = File.createTempFile(file, " .png");
+    ImageIO.write(image, "png", tempFile);
+    bucketComponent.upload(tempFile, "images/");
+    return tempFile;
+  }
 
-    BufferedImage result =
-        new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
-    result.getGraphics().drawImage(image, 0, 0, null);
-    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-    ImageIO.write(result, "png", byteArrayOutputStream);
-    return byteArrayOutputStream.toByteArray();
+  public void ConvertSary(byte[] sary, String id) {
+
+    try {
+      if (sary == null) {
+        throw new RuntimeException("Select a image");
+      }
+      BufferedImage image = ImageIO.read(new ByteArrayInputStream(sary));
+
+      uploadImage(sary + ".orginal", image);
+      BufferedImage result =
+          new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+      result.getGraphics().drawImage(image, 0, 0, null);
+
+      uploadImage(sary + "blackwhite", image);
+
+    } catch (IOException e) {
+      e.printStackTrace();
+      throw new RuntimeException("Error 500");
+    }
   }
 }
